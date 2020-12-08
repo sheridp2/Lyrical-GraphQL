5bff9893a4e07b620c2427ef316bbba2312f0236
Patrick Sheriddan <sheridp2@gmail.com>
2020-12-07 17:07:45 -0800

    lyrics submiting successfully to db

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
 import fetchSongDetails from '../queries/fetchSongDetails'
+import LyricCreate from './LyricCreate'
 
 class SongDetail extends Component {
   render() {
+    const { song } = this.props.data
+
+    if(!song){
+      return <div>Loading...</div>
+    }
+
     return(
       <div>
-        <h3>Song Detail</h3>
+        <Link to="/" className="btn light-blue lighten-2">Back</Link>
+        <h3>{song.title}</h3>
+        <LyricCreate songId={this.props.params.id} />
       </div>
     )
   }
